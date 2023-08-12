ddc8399b672cb724d7569767c7aa8c5b0cd86cb9
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2023-08-12 11:10:06 +0200

    Refactoring getHtml() to use forEach()

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ tweetBtn.addEventListener("click", function () {
 function getFeedHtml() {
   let feedHtml = ``;
 
-  for (let tweet of tweetsData) {
+  tweetsData.forEach(function (tweet) {
     feedHtml += `<div class="tweet">
     <div class="tweet-inner">
         <img src="${tweet.profilePic}" class="profile-pic">
@@ -31,8 +31,8 @@ function getFeedHtml() {
         </div>            
     </div>
 </div>`;
-  }
+  });
   return feedHtml;
 }
 
-getFeedHtml();
+console.log(getFeedHtml());
